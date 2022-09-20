61b641c62b5cb31be60dd737367a65a3f1d1cf6e
[Ruliaga] <ruliaga@mail.ru>
2022-09-20 23:36:27 +0500

    Task66 done

---
+++ Task66/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+Console.WriteLine("Введите M:");
+int m=int.Parse(Console.ReadLine());
+Console.WriteLine("Введите N:");
+int n=int.Parse(Console.ReadLine());
+Console.WriteLine("------------------");
+Console.Write($"M={m}; N={n} ->");
+Function(m,n,0);
+
+void Function (int m, int n, int sum){
+
+if ((m<=n)){
+        sum=sum+m;
+        Function(m+1,n,sum);
+} else {Console.Write(sum);}
+
+}
