09c727ee79df79701e4308845b3e9115a02662ec
[Ruliaga] <ruliaga@mail.ru>
2022-09-20 23:04:01 +0500

    Task64 done

---
+++ Task64/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+Console.WriteLine("Введите M:");
+int m=int.Parse(Console.ReadLine());
+Console.WriteLine("Введите N:");
+int n=int.Parse(Console.ReadLine());
+
+Function(m,n);
+
+void Function (int m, int n){
+
+if ((m<=n)&(m%2==0)){
+        Console.Write($"{m} ");
+        Function(m+1,n);
+} 
+if ((m<=n)&(m%2!=0)){
+        Console.Write(" ");
+        Function(m+1,n);
+
+}
+}
